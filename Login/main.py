9dc4770cc4a98c338fc8928e0145d7f12e9bd0f4
Ali Örer <129732909+AliOrer@users.noreply.github.com>
2025-10-09 18:17:30 +0200

    number guessing game  in python added

--- Login/main.py
+++ Login/main.py
@@ -1,1 +1,16 @@
- 
+ import random
+
+ n = random.randrange(1, 10)
+
+ guess = int(input("enter a number: "))
+
+ while n! = guess:
+    if guess < n:
+        print("too low")
+        guess = int(input("enter a number again: "))
+    elif guess > n:
+        print("too high!")
+        guess = int(input("enter number again: "))
+    else:
+        break
+    print("you guessed it right!!")
