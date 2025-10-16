4ea44b874a30ea0a45a48ac09b93ebfc8ab95275
Ali Örer <129732909+AliOrer@users.noreply.github.com>
2025-10-16 06:36:51 +0200

    random ve string kütüphaneleri çekilerek sifre üretici fontsiyon ollusturuldu. 16 hanelik sifre üretici değişkeni atandı, ardından karakterler değiskenin içeriği olusturuldu. karakter özel simgeler,sayılar ve noktalama isaretlerini kullanarak random 16 harflik oluşturulacak. ve bu harfler for döngüsü içinde 16 hanelik olmak üzere bütün 'karakterler' içeriğinden random bir biçimde çekilereck. ve print ile cağrılıan fonksiyonu sectiğimiz karakter sayısına göre  ekrana yazdıracak.

--- sifre-olusturucu/main.py
+++ sifre-olusturucu/main.py
@@ -1,1 +1,12 @@
 
+import random , string
+
+def sifre_uret(uzunluk =16):
+    karakterler = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(karakterler) 
+
+for i in range(uzunluk))
+
+print("rastgele sifre : " , sifre_uret(17))
+
+
