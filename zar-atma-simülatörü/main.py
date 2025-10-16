4b49a4319d51f98dc861411d335e6734064a401f
Ali Örer <129732909+AliOrer@users.noreply.github.com>
2025-10-16 06:27:54 +0200

    random import edilerek , while döngüsünde 1-6 arası rastgele  zar numaları ekrana yazdırıld, kullanıcı zar sonucunu gördüğünde evet-hayr sorusuyla karşılaşacak, verdiği evet sonucu döngüyü devam ettirrirken, hayır sonucu ile döngü break komutuyla kırılacak.

--- zar-atma-simülatörü/main.py
+++ zar-atma-simülatörü/main.py
@@ -1,1 +1,8 @@
-internetim yok, sari bozulmasın 
+import random
+
+while True:
+    zar = random.randint(1, 6)
+    print('zar sonucu', zar)
+    devam = input('tekrar atmak ister misin? (e/h): ')
+    if devam.lower() != 'e':
+        break
