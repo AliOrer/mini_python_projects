71b25595e44d47d444cc01bb0c3635557eec4243
Ali Örer <129732909+AliOrer@users.noreply.github.com>
2025-10-21 10:15:46 +0200

    to-do list structure built

--- to-do-list/main.py
+++ to-do-list/main.py
@@ -1,11 +1,26 @@
 todo = []
 
 while True:
-  print('\n1. gorv ekle\n2.gorevleri gor\n3. sil\n4. cikis')
-  secim = input("secimniz: ")
-
-
-   if secim == '1':
-    gorev = input("yeni gorev: ")
+  print("\n1.Gorev Ekle\n2.Gorevleri Gor\n3.Sil\n4.Cikis")
+  secim = input("Seciminiz: ")
+  
+  if secim == '1':
+    gorev = input("Yeni Gorev: ")
     todo.append(gorev)
+
+  elif secim == '2':
+
+    for i, g in enumerate(todo, 1):
+      print(f"{i}, {g}")
+
+  elif secim == '3':
+    sil = int(input("Silmek Istediginiz Gorev Numarasi: "))
+
+    if 0 < sil <= len(todo):
+      todo.pop(sil-1)
+
+  elif secim == '4':
+    break
     
+  else:
+    print("gecersiz bir secim yaptinizz !")
